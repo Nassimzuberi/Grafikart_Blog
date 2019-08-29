f2dab80ea2db051a7c3b3b788ae923e963546f18
ZUBERI Nassim <nassim.zuberi@hotmail.fr>
2019-08-29 13:07:14 +0200

    Commit du dossier pages

--- pages/home.php
+++ pages/home.php
@@ -1,14 +1,30 @@
-<ul>
 
-<?php foreach($db->query('SELECT * FROM articles',('App\Table\Article')) as $post): ?>
-<h2>
-    <a href="<?= $post->url ; ?>"><?= $post->titre ; ?></a>
-</h2>
-<p><?= $post->extrait; ?></p>
+<div class="row">
+    <div class="col-sm-8">
+        <?php foreach(App\Table\Article::getLast() as $post): ?>
+            <h2>
+                <a href="<?= $post->url ; ?>"><?= $post->titre ; ?></a>
+            </h2>
+            <p> <em>
+                    <?= $post->categorie; ?>
+                </em></p>
+            <p><?= $post->extrait; ?></p>
 
-<?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
+    <div class="col-sm-4">
+    <ul>
 
-</ul>
+         <?php foreach(App\Table\Categorie::all() as $categories): ?>
+            <li><a href="<?= $categories->url; ?>"><?= $categories->titre; ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+
+</div>
+
+
+
     <?php
     // $count = $pdo->exec('insert into articles SET titre = "Mon titre", date ="'.date('Y-m-d H:i:s').'"');
 
